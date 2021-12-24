9f116dd39cf80739aafee760726153649cbcc482
lennard-geeraert <lennard.geeraert@student.hogent.be>
2021-12-24 15:51:34 +0100

    Initial commit

---
+++ quizapp/quizappApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizappApp.swift
+//  quizapp
+//
+//  Created by Francis Geeraert on 24/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct quizappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
